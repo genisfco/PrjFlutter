1e974d5d492312d0746a13003ddbd90abd546501
GenisCarvalho <genis_carvalho@hotmail.com>
2025-12-01 13:20:46 -0300

    feat: Update project configuration and implement Windows support
    - Updated project metadata with new revision and migration details.
    - Modified Botoes widget to accept a callback function for button presses.
    - Enhanced Home widget to include a list of questions and handle responses.
    - Updated pubspec.lock to reflect new package versions.
    - Added Windows-specific files including CMakeLists, resource files, and utility functions for console handling.
    - Implemented Win32Window class for managing the application window.
    - Added support for dark mode and DPI scaling in the Windows application.
    - Created necessary resource files for application icon and versioning.

--- lib/botoes.dart
+++ lib/botoes.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Botoes extends StatelessWidget {
-  const Botoes({super.key});
+  const Botoes({required this.resp, super.key});
+
+  final void Function()resp;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: resp,
         style: ElevatedButton.styleFrom(
           backgroundColor: Colors.blue,
           foregroundColor: Colors.white,
--- lib/main.dart
+++ lib/main.dart
@@ -25,21 +25,47 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home>  {
+ final perguntas = [
+  'Qual sua cor favorita?',
+  'Qual o seu animal favorito?',
+  'Qual seu esporte favorito?',
+  'Qual é o seu time?',
+  'Qual é o seu hobby?',
+ ];
+
+ var indicePergunta = 0;
+
+ void responder() {
+  if (indicePergunta < perguntas.length - 1) {
+    indicePergunta++;
+  } else {
+    indicePergunta = 0;
+  }
+  setState(() {});
+ }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Perguntas'),
+        title: const Text('Genis Ferreira', style: TextStyle(fontSize: 30)),
         centerTitle: true,
+        backgroundColor: Colors.amber,
+        toolbarHeight: 80,
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Column(
           children: [
-            Text("Perguntas", style: TextStyle(fontSize: 40),),
-            Botoes(),
-            Botoes(),
-            Botoes(),
+            Text(
+              perguntas[indicePergunta],
+              style: TextStyle(fontSize: 30)
+            ),
+            Botoes(resp: responder),
+            Botoes(resp: responder),
+            Botoes(resp: responder),
+            Botoes(resp: responder),
+            Botoes(resp: responder),
           ],
         ),
       ),
